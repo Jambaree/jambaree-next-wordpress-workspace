edc79ddbe9e2e886a1a8a7260134b441ec497c69
Rene Sauve <rdasauve@gmail.com>
2023-01-25 13:34:26 -0800

    wrong import name for flexiblecontent

--- @jambaree/next-wordpress/index.ts
+++ @jambaree/next-wordpress/index.ts
@@ -2,7 +2,7 @@ import getSeedData from "./src/getSeedData";
 import getTemplate from "./src/getTemplate";
 import getContentType from "./src/getContentType";
 import WordpressTemplate from "./src/WordpressTemplate";
-import FlexibleContent from "./src/FlexibleContent";
+import FlexibleContent from "./src/flexibleContent";
 import getMenuItems from "./src/getMenuItems";
 import YoastSEO from "./src/YoastSEO";
 
