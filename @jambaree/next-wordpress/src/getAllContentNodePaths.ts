96fa7a6c7afbc5dcec7ee410103ba269bbb8c2aa
Rene Sauve <rdasauve@gmail.com>
2023-03-14 11:48:58 -0700

    change version in package.json

--- @jambaree/next-wordpress/src/getAllContentNodePaths.ts
+++ @jambaree/next-wordpress/src/getAllContentNodePaths.ts
@@ -19,7 +19,7 @@ const getAllContentNodePaths = async ({ url }: { url?: string }) => {
     url: url || process.env.NEXT_PUBLIC_WPGRAPHQL_URL || "",
     document: queryDocument,
   });
-
+  // @ts-expect-error
   const typeSlugs = res?.contentNodes?.nodes;
 
   return typeSlugs;
