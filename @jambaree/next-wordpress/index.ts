918608961ec60eae4074765dba1bd242b56ddd4f
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-03-17 15:42:49 -0700

    only export what is needed / combine into export from statements

--- @jambaree/next-wordpress/index.ts
+++ @jambaree/next-wordpress/index.ts
@@ -1,23 +1,7 @@
-import getSeedData from "./src/getSeedData";
-import getTemplate from "./src/getTemplate";
-import getAllContentNodePaths from "./src/getAllContentNodePaths";
-import WordpressTemplate from "./src/WordpressTemplate";
-import FlexibleContent from "./src/flexibleContent";
-import useMenuItems from "./src/useMenuItems";
-import getData from "./src/getData";
-import getYoastData from "./src/getYoastData";
-import { generateStaticParams } from "./src/generateStaticParams";
-import { generateMetadata } from "./src/generateMetadata";
-
-export {
-  getYoastData,
-  generateStaticParams,
-  generateMetadata,
-  getData,
-  getSeedData,
-  getTemplate,
-  getAllContentNodePaths,
-  WordpressTemplate,
-  FlexibleContent,
-  useMenuItems,
-};
+export { generateStaticParams } from "./src/generateStaticParams";
+export { getSeedData } from "./src/getSeedData";
+export { getMenuItems } from "./src/getMenuItems";
+export { generateMetadata } from "./src/generateMetadata";
+export { FlexibleContent } from "./src/flexibleContent";
+export { getData } from "./src/getData";
+export { WordpressTemplate } from "./src/WordpressTemplate";
