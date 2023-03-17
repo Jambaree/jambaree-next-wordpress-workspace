5bb2b61ac2ad462dc17c3f61c8e8f88f6a4ae871
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-03-17 16:01:11 -0700

    getTemplate - add error logs for template not found on archives and taxonomy

--- @jambaree/next-wordpress/src/getTemplate.ts
+++ @jambaree/next-wordpress/src/getTemplate.ts
@@ -1,4 +1,3 @@
-import chalk from "chalk";
 import log from "./utils/log";
 
 type GetTemplateArgs = {
@@ -35,12 +34,37 @@ export default async function getTemplate({
   const isArchive = seedData?.__typename === "ContentType";
 
   if (isArchive) {
-    const template = templates?.archive?.[toCamel(seedData?.graphqlSingleName)];
+    const templateName = toCamel(seedData?.graphqlSingleName);
+    const template = templates?.archive?.[templateName];
+
+    if (!template) {
+      log(
+        `Warn: Template "${templateName}" not found for type "${seedData?.__typename}" (${templateName} archive) on uri '${seedData?.uri}'. Did you forget to add it to the templates object in src/templates/index? `
+      );
+
+      log(seedData, {
+        prefix: "seedData",
+        prefixColor: "cyan",
+        separator: ":",
+      });
+    }
     return template;
   }
 
   if (seedData?.isTermNode) {
     const template = templates?.taxonomy?.[`${seedData?.taxonomyName}`];
+
+    if (!template) {
+      log(
+        `Warn: Template "${seedData?.taxonomyName}" that would be used for term "${seedData?.slug}" not found for taxonomy "${seedData?.__typename}" on uri '${seedData?.uri}'. Did you forget to add it to the templates object in src/templates/index? `
+      );
+
+      log(seedData, {
+        prefix: "seedData",
+        prefixColor: "cyan",
+        separator: ":",
+      });
+    }
     return template;
   }
 
