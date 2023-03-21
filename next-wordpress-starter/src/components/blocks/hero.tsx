7e4ff0d5beb3692652e30c32375ef19088f6d85d
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-03-21 12:29:41 -0700

    Update hero.tsx

--- next-wordpress-starter/src/components/blocks/hero.tsx
+++ next-wordpress-starter/src/components/blocks/hero.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export function Hero(props) {
   return (
     <div>
-      <h1>Hero</h1>
+      <span className="bg-orange-500 rounded-lg border-orange-300 px-3">
+        Hero
+      </span>
+
+      <h1>{props.headline}</h1>
+
+      <Image
+        src={props.image.sourceUrl}
+        alt={props.image.altText}
+        width={props.image.mediaDetails.width}
+        height={props.image.mediaDetails.height}
+      />
+
       <pre>
         <code>{JSON.stringify({ props }, null, 2)}</code>
       </pre>
