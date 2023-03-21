3a5f83b1c2c243c0ea25331b3d07e76d6b135c76
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-03-21 12:29:36 -0700

    add wpBaseUrl to next image config

--- next-wordpress-starter/next.config.js
+++ next-wordpress-starter/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const wpBaseUrl = process.env.NEXT_PUBLIC_WP_URL?.replace("https://", "");
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 
   images: {
-    domains: ["images.unsplash.com", "tailwindui.com"],
+    domains: ["images.unsplash.com", "tailwindui.com", wpBaseUrl],
   },
 };
 
