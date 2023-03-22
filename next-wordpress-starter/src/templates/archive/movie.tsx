9fbc2df273e2869768a4ac7a1fe35f6263bc5045
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-03-22 15:01:40 -0700

    add movie sample query

--- next-wordpress-starter/src/templates/archive/movie.tsx
+++ next-wordpress-starter/src/templates/archive/movie.tsx
@@ -8,6 +8,7 @@ export default async function MovieArchive() {
           nodes {
             id
             title
+            uri
           }
         }
       }
--- next-wordpress-starter/src/templates/movie/default.tsx
+++ next-wordpress-starter/src/templates/movie/default.tsx
@@ -1,3 +1,33 @@
-export default async function DefaultMovieTemplate({ uri }) {
-  return <div>Movie {uri}</div>;
+import { getData } from "@jambaree/next-wordpress";
+
+export default async function DefaultMovieTemplate({
+  uri,
+  isPreview,
+  searchParams,
+}) {
+  const data = await getData({
+    variables: { id: uri, idType: "URI" },
+    query,
+    isPreview,
+    searchParams,
+  });
+
+  return (
+    <div>
+      Movie uri: {uri}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
+
+const query = /* GraphQL */ `
+  query MovieQuery($id: ID!, $idType: MovieIdType) {
+    movie(id: $id, idType: $idType) {
+      __typename
+      id
+      title
+      uri
+      slug
+    }
+  }
+`;
