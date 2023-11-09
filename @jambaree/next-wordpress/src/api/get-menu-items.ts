d4716ff555b5d679c4b180b63d53d393f3c738d8
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-11-08 17:46:28 -0800

    structure menu items with childItems instead of flat

--- @jambaree/next-wordpress/src/api/get-menu-items.ts
+++ @jambaree/next-wordpress/src/api/get-menu-items.ts
@@ -28,13 +28,48 @@ export async function getMenuItems({ slug }: { slug: string }): Promise<
     args
   );
 
-  const data = await req.json();
+  let data;
+  try {
+    data = await req.json();
+  } catch (err) {
+    throw new Error(`Error fetching menu items: ${err.message}`);
+  }
 
-  return data?.map?.((menuItem) => {
+  const flatMenu = data?.map?.((menuItem) => {
     return {
       ...menuItem,
       label: menuItem?.title?.rendered,
       path: menuItem.url.replace(process.env.NEXT_PUBLIC_WP_URL, ""),
     };
   });
+
+  return structureMenuItems(flatMenu);
+}
+
+function structureMenuItems(menuItems) {
+  // Create a map for easy lookup of child items
+  const itemsById = new Map(
+    menuItems.map((item) => [item.id, { ...item, childItems: [] }])
+  );
+
+  // The final structured menu items
+  const structuredItems = [];
+
+  // Iterate over the menu items to structure them
+  for (const item of menuItems) {
+    const structuredItem = itemsById.get(item.id);
+
+    if (item.parent === 0) {
+      // This is a top-level menu item
+      structuredItems.push(structuredItem);
+    } else {
+      // This item is a child of another item
+      const parentItem = itemsById.get(item.parent);
+      if (parentItem) {
+        parentItem.childItems.push(structuredItem);
+      }
+    }
+  }
+
+  return structuredItems;
 }
