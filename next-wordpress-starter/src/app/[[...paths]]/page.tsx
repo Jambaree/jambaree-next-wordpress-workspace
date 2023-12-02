550c7f3da129671f32f9cceddab71a8f486228a5
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-01 22:38:56 -0800

    Revert "Update page.tsx"
    This reverts commit 71f72b0dc9f534b9b08b13e93d6a3800eb7d9672.

--- next-wordpress-starter/src/app/[[...paths]]/page.tsx
+++ next-wordpress-starter/src/app/[[...paths]]/page.tsx
@@ -9,15 +9,11 @@ export default function PageRoute({
   searchParams?: Record<string, string | string[] | undefined>;
 }) {
   return (
-    <>
-      <pre>{JSON.stringify({ params }, null, 2)}</pre>
-      <pre>{JSON.stringify({ searchParams }, null, 2)}</pre>
-      <WordpressTemplate
-        params={params}
-        searchParams={searchParams}
-        templates={templates}
-      />
-    </>
+    <WordpressTemplate
+      params={params}
+      searchParams={searchParams}
+      templates={templates}
+    />
   );
 }
 
