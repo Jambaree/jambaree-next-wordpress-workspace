ae8f58337a4f726ade2ab69b0a6ec4eb31c0dc71
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-01 23:07:13 -0800

    Update page.tsx

--- next-wordpress-starter/src/app/[[...paths]]/page.tsx
+++ next-wordpress-starter/src/app/[[...paths]]/page.tsx
@@ -1,17 +1,14 @@
 import { WordpressTemplate } from "@jambaree/next-wordpress";
 import templates from "@/templates";
 
-export default function PageRoute({
-  params,
-  searchParams,
-}: {
+export default function PageRoute(props: {
   params: { paths: string[] };
   searchParams?: Record<string, string | string[] | undefined>;
 }) {
   return (
     <WordpressTemplate
-      params={params}
-      searchParams={searchParams}
+      params={props.params}
+      searchParams={props.searchParams}
       templates={templates}
     />
   );
